b6e3cff13e35f6f8aa47556438b508df2db1aafb
Inpek <OpenDyto@protonmail.com>
2022-05-20 21:25:22 +0200

    17

--- Tuturu/budget/views.py
+++ Tuturu/budget/views.py
@@ -1,7 +1,12 @@
-#from django.http import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
+from budget.models import Article
 # Create your views here.
 def index(request):
+    article_obj = Article.objects.get(id=1)
+    article_title = article_obj.title
+    article_content = article_obj.content
+    STRING = f"""<h1>{article_title}</h1>"""
     return render(request, "budget/index.html")
 
 def sd(request, numero_sd):
